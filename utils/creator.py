cbb3759b1b7b0287dea8eb289c0e5ec3e4177554
billpsomas <psomasbill@gmail.com>
2021-09-02 17:10:06 +0300

    more changes

--- utils/creator.py
+++ utils/creator.py
@@ -1,12 +1,10 @@
-import glob
 import os
 import argparse
-from pathlib import Path
-from collections import defaultdict
 import csv
 import json
-import random
 import pdb
+
+from collections import defaultdict
 
 class ActionLabels(object):
     """
@@ -24,7 +22,49 @@ class ActionLabels(object):
         "No action ball": 7
     }
 
+def read_csv(csv_file):
+    '''
+    Given an AVA-like CSV file for training or validation, extract info about video/frame paths/ids, append to list and return
+    Args:
+        csv_file: The CDV file to read from (i.e. train.csv or val.csv)
+    '''
+    video_frame_ids = []
+    with open(csv_file) as f:
+        csv_reader = csv.reader(f, delimiter=' ')
+        for i, row in enumerate(csv_reader):
+            if i > 0:
+                video_frame_ids.append([row[0], row[4].split('/')[1], row[2]])
+
+    return video_frame_ids
+
+def read_json(json_file):
+    '''
+    Given a Mindy Support anotation JSON file, create a dictionary having the bounding boxes infomation for each frame of each video
+    Args:
+        json_file: The JSON file to read from (i.e. 2021-04-27_10-01-15.json)
+    '''
+    with open(os.path.join(json_path, json_file), 'r') as file:
+        annotation_data = json.load(file)
+
+        boxes = defaultdict(list)
+        for annotation in annotation_data['annotations']:
+            img = annotation_data['images'][annotation['image_id']-1]['file_name']
+            bbox = annotation['bbox']
+            action_class = ActionLabels.ACTION_CLASSES_TO_IDS[annotation['attributes']['action_class']]
+            
+            boxes[img].append([bbox[0]/2592, bbox[1]/1944, bbox[2]/2592, bbox[3]/1944, action_class])
+    
+    return boxes
+
 def write_frames(video_frame_ids, video_frame_boxes, csv_file):
+    '''
+    Given a list having the video/frame paths/ids (from read_csv) and a dictionary having the bounding boxes infomation for each frame of each video (from read_json),
+    writes all the needed info as it should to csv_file 
+    Args:
+        video_frame_ids: list having the video/frame paths/ids (from read_csv)
+        video_frame_boxes: dictionary having the bounding boxes infomation for each frame of each video (from read_json)
+        csv_file: the file to write to (i.e. ava_train_v2.2, ava_val_v2.2)
+    '''
     with open(os.path.join(output_path, csv_file), 'w') as f:
         writer = csv.writer(f, delimiter=',', quotechar="'")
         for video_frame in video_frame_ids:
@@ -35,45 +75,6 @@ def write_frames(video_frame_ids, video_frame_boxes, csv_file):
                 writer.writerow([video, id, bb[0], bb[1], bb[2], bb[3], bb[4]])
 
     return None
-
-def read_csv(csv_file):
-    '''
-    Given an AVA-like CSV file for training or validation, extract info about video/frame paths/ids, append to list and return
-    Args:
-        csv_file: The file to read from (i.e. train.csv or val.csv)
-    '''
-    #original_video_ids = []
-    #original_frame_ids = []
-    #frame_ids = []
-    video_frame_ids = []
-    with open(csv_file) as f:
-        csv_reader = csv.reader(f, delimiter=' ')
-        for i, row in enumerate(csv_reader):
-            if i > 0:
-                video_frame_ids.append([row[0], row[4].split('/')[1], row[2]])
-                #original_video_ids.append(row[0])
-                #original_frame_ids.append(row[4].split('/')[1])
-                #frame_ids.append(row[2])
-
-    return video_frame_ids
-
-def read_json(json_file):
-    with open(os.path.join(json_path, json_file), 'r') as file:
-        annotation_data = json.load(file)
-
-        boxes = defaultdict(list)
-        for annotation in annotation_data['annotations']:
-            img = annotation_data['images'][annotation['image_id']-1]['file_name']
-            bbox = annotation['bbox']
-            #object_class = annotation['category_id']
-            action_class = ActionLabels.ACTION_CLASSES_TO_IDS[annotation['attributes']['action_class']]
-            
-            boxes[img].append([bbox[0], bbox[1], bbox[2], bbox[3], action_class])
-            
-            #boxes.append([json_file, img, bbox[0], bbox[1], bbox[2], bbox[3], action_class])
-    
-    return boxes
-
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Annotation files creator. Takes as input the JSON files of Mindy and the CSV files of AVA dataset. Outputs ava_train_v2.2 and ava_val_v2.2 CSV files')
@@ -90,19 +91,28 @@ if __name__=="__main__":
     print('Reading training and validation CSVs')
     train_video_frame_ids = read_csv(os.path.join(csv_path, 'train.csv'))
     val_video_frame_ids = read_csv(os.path.join(csv_path, 'val.csv'))
+    print('Found {} frames for training and {} frames for validation.'.format(len(train_video_frame_ids), len(val_video_frame_ids)))
+    print('Total number of frames from CSVs: {}'.format(len(train_video_frame_ids) + len(val_video_frame_ids)))
 
     # Read JSONs
     jsons = [x for x in os.listdir(json_path) if x.endswith('.json')]
     print('Found {} JSON files in given directory.'.format(len(jsons)))
     
     video_frame_boxes = defaultdict(list)
+    sum_boxes = 0
     for json_file in jsons:
         print('Reading {} file...'.format(json_file))
         per_json_boxes = read_json(json_file)
-        print('Found {} boxes in it.'.format(len(per_json_boxes)))
+        print('Found {} frames in it.'.format(len(per_json_boxes)))
         video_frame_boxes[json_file.split('.')[0]].append(per_json_boxes)
 
+        sum_boxes += len(per_json_boxes)
+    
+    print('Total number of frames from JSONs: {}'.format(sum_boxes))
+
+    assert len(train_video_frame_ids) + len(val_video_frame_ids) == sum_boxes, "Oh no! Number of frames in CSVs and JSONs not consistent..."
+
+    # Write to CSV files
+    print('Writing to CSV files...')
     write_frames(train_video_frame_ids, video_frame_boxes, 'ava_train_v2.2.csv')
     write_frames(val_video_frame_ids, video_frame_boxes, 'ava_val_v2.2.csv')
-
-
